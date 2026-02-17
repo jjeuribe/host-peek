bde7f15a5de5289845bc809f40e4c47f41456f75
turboaux <jje.uribe@gmail.com>
2026-02-16 20:15:23 -0600

    incorporate a option menu

--- hostpeek.sh
+++ hostpeek.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-
+red='\e[31m'
+green='\e[32m'
+blue='\e[34m'
+clear='\e[0m'
 host_name=$(hostname)
+
+function color_red() {
+    echo -ne $red$1$clear
+}
+
+function color_green() {
+    echo -ne $green$1$clear
+}
+
+function color_blue() {
+    echo -ne $blue$1$clear
+}
 
 function memory_check() {
     echo ""
@@ -33,7 +48,24 @@ function kernel_check() {
     echo ""
 }
 
-memory_check
-cpu_check
-tcp_check
-kernel_check
+function main_menu() {
+    echo -e $(color_green '1)') Memory usage
+    echo -e $(color_green '2)') CPU load
+    echo -e $(color_green '3)') Number of TCP connections
+    echo -e $(color_green '4)') Kernel version
+    echo -e $(color_green '0)') Exit
+    echo -ne $(color_blue 'Choose an option: ')
+
+    read menu_option
+
+    case $menu_option in
+        1) memory_check; main_menu;;
+        2) cpu_check; main_menu;;
+        3) tcp_check; main_menu;;
+        4) kernel_check; main_menu;;
+        0) exit 0;;
+        *) echo -e $(color_red "Wrong option!"); main_menu;;
+    esac
+}
+
+main_menu
