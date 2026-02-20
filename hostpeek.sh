f44b0ee65d8d4f5c9851ef623a1920bbba96d968
turboaux <jje.uribe@gmail.com>
2026-02-19 19:11:58 -0600

    rename metrics.sh to system_metrics.sh

--- hostpeek.sh
+++ hostpeek.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 LIB_DIR="${SCRIPT_DIR}/lib"
 
 source "${LIB_DIR}/colors.sh"
-source "${LIB_DIR}/metrics.sh"
+source "${LIB_DIR}/system_metrics.sh"
 HOSTNAME=$(hostname)
 
 function label() {
